af9964aa5750d36c185c8a8ad0d18b58b5d179d0
Edmond Chuc <edmond.chuc@csiro.au>
2019-02-07 11:26:21 +1000

    Added human-readable URLs to class instance URIs.

--- ldcat/helper.py
+++ ldcat/helper.py
@@ -3,6 +3,10 @@ from rdflib import URIRef
 import markdown
 
 
+def make_title(s):
+    return ' '.join(s.split('#')[-1].split('/')[-1].split('_')).title()
+
+
 def parse_markdown(s):
     return markdown.markdown(s)
 
